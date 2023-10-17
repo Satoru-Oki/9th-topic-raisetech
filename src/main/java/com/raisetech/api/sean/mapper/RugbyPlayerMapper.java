b850843ed969ade2d88eb350f93b72f0191f7347
Satoru-Oki <columbo0730@gmail.com>
2023-10-17 21:40:54 +0900

    コメントアウトの削除

--- src/main/java/com/raisetech/api/sean/mapper/RugbyPlayerMapper.java
+++ src/main/java/com/raisetech/api/sean/mapper/RugbyPlayerMapper.java
@@ -6,21 +6,6 @@ import org.apache.ibatis.annotations.SelectProvider;
 
 import java.util.List;
 
-//@Mapper
-//public interface RugbyPlayerMapper {
-//
-//    @Select("SELECT * FROM rugbyPlayers")
-//    List<RugbyPlayer> findAll();
-//
-//    @Select("<script>" +
-//            "SELECT * FROM rugbyPlayers " +
-//            "WHERE 1=1 " +
-//            "<if test='height != null'>AND height > #{height}</if> " +
-//            "<if test='weight != null'>AND weight > #{weight}</if> " +
-//            "<if test='rugbyPosition != null'>AND rugbyPosition = #{rugbyPosition}</if> " +
-//            "</script>")
-//    List<RugbyPlayer> findByReference(Integer height, Integer weight, String rugbyPosition);
-//}
 @Mapper
 public interface RugbyPlayerMapper {
 
